2ab96489c687c4e6ca4708838266baa561c007ff
makhatadze <vitomaxatadze@gmail.com>
2020-09-01 11:12:42 +0400

    Add laravel localization package.

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 
 //HOME CONTROLLER
-Route::group(['prefix' => '/'], function () {
-    Route::any('/home',['uses'=>'MainController@index', 'as' => 'ActionMain']);
-    Route::get('/user', ['uses'=>'UserController@index', 'as' => 'ActionUser']);
-    Route::get('/services', ['uses'=>'ServiceController@index', 'as' => 'ActionService']);
-    Route::get('/products', ['uses'=>'ProductController@index', 'as' => 'ActionProduct']);
-    Route::get('/clients', ['uses'=>'ClientController@index', 'as' => 'ActionClient']);
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['web', 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function(){
+    Route::group(['prefix' => '/'], function () {
+        Route::any('/home',['uses'=>'MainController@index', 'as' => 'ActionMain']);
+        Route::get('/user', ['uses'=>'UserController@index', 'as' => 'ActionUser']);
+        Route::get('/services', ['uses'=>'ServiceController@index', 'as' => 'ActionService']);
+        Route::get('/products', ['uses'=>'ProductController@index', 'as' => 'ActionProduct']);
+        Route::get('/clients', ['uses'=>'ClientController@index', 'as' => 'ActionClient']);
+    });
 });
