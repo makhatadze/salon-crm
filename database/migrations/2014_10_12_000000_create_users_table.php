ba00207b679d44af6c36f7546658a7bcf689bced
makhatadze <vitomaxatadze@gmail.com>
2020-09-10 13:49:08 +0400

    Change roles migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->timestamp('deleted_at')->nullable();
+
         });
     }
 
