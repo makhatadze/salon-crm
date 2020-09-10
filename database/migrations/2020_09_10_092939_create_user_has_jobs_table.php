830cf864e7a999d5565d330e691ede0c6764d0dd
makhatadze <vitomaxatadze@gmail.com>
2020-09-10 13:43:12 +0400

    Create user has jobs model and migration.

---
+++ database/migrations/2020_09_10_092939_create_user_has_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserHasJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_has_jobs', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('company_id')->unsigned()->index();
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->bigInteger('office_id')->unsigned()->index();
+            $table->foreign('office_id')->references('id')->on('offices');
+            $table->bigInteger('department_id')->unsigned()->index();
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_has_jobs');
+    }
+}
