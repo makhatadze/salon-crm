641d59e332526663fa73bfcca3a3c459ba4a7f04
makhatadze <vitomaxatadze@gmail.com>
2020-09-22 15:54:40 +0400

    Add filter to company/finances.

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -9,6 +9,7 @@
  */
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Service;
 use App\ClientService;
@@ -17,6 +18,8 @@ use App\Product;
 use App\Image;
 use App\Inventory;
 use Carbon\Carbon;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+
 class ServiceController extends Controller
 {
     /**
@@ -58,8 +61,8 @@ class ServiceController extends Controller
             'editor-ge' => 'required',
             'editor-en' => '',
             'editor-ru' => '',
-            'duration_count' => 'required|between:0,99.99', 
-            'duration_type' => 'required|string', 
+            'duration_count' => 'required|between:0,99.99',
+            'duration_type' => 'required|string',
             'price' => 'required|between:0,9999.99',
             'unit-ge' => '',
             'unit-en' => '',
@@ -67,51 +70,51 @@ class ServiceController extends Controller
             'file' => 'image',
             'inventory' => '',
             'quantity' => '',
-         ]);
-         $service = new Service;
-         $service->title_ge = $request->input('title_ge');
-         $service->title_en = $request->input('title_en');
-         $service->title_ru = $request->input('title_ru');
-         $service->body_ge = $request->input('editor-ge');
-         $service->body_en = $request->input('editor-en');
-         $service->body_ru = $request->input('editor-ru');
-         $service->duration_count = $request->input('duration_count');
-         $service->duration_type = $request->input('duration_type');
-         $service->unit_ge = $request->input('unit-ge');
-         $service->unit_ru = $request->input('unit-ru');
-         $service->unit_en = $request->input('unit-en');
-         $service->price = intval($request->input('price')*100);
+        ]);
+        $service = new Service;
+        $service->title_ge = $request->input('title_ge');
+        $service->title_en = $request->input('title_en');
+        $service->title_ru = $request->input('title_ru');
+        $service->body_ge = $request->input('editor-ge');
+        $service->body_en = $request->input('editor-en');
+        $service->body_ru = $request->input('editor-ru');
+        $service->duration_count = $request->input('duration_count');
+        $service->duration_type = $request->input('duration_type');
+        $service->unit_ge = $request->input('unit-ge');
+        $service->unit_ru = $request->input('unit-ru');
+        $service->unit_en = $request->input('unit-en');
+        $service->price = intval($request->input('price')*100);
 
-        
-         $service->save();
-         $array = array();
-         if($request->input('inventory') && $request->input('quantity')){
-             foreach($request->input('inventory') as $key => $item){
-                 $array[] =[
+
+        $service->save();
+        $array = array();
+        if($request->input('inventory') && $request->input('quantity')){
+            foreach($request->input('inventory') as $key => $item){
+                $array[] =[
                     'product_id' => $request->input('inventory')[$key],
                     'quantity' => $request->input('quantity')[$key],
-                 ];
-             }
-             $service->inventories()->createMany($array);
-         }
-         if($request->input('category-ge') || $request->input('category-en') || $request->input('category-ru')){
+                ];
+            }
+            $service->inventories()->createMany($array);
+        }
+        if($request->input('category-ge') || $request->input('category-en') || $request->input('category-ru')){
 
             $service->category()->create([
                 'title_ge' => $request->input('category-ge'),
                 'title_ru' => $request->input('category-ru'),
                 'title_en' => $request->input('category-en'),
-             ]);
-         }
-         
-         if($request->hasFile('file')){
-             $imagename = date('Ymhs').$request->file('file')->getClientOriginalName();
-             $destination = base_path() . '/storage/app/public/serviceimg';
-             $request->file('file')->move($destination, $imagename);
-             $service->image()->create([
-                 'name' => $imagename
-             ]);
-         }
-         return redirect('/services');
+            ]);
+        }
+
+        if($request->hasFile('file')){
+            $imagename = date('Ymhs').$request->file('file')->getClientOriginalName();
+            $destination = base_path() . '/storage/app/public/serviceimg';
+            $request->file('file')->move($destination, $imagename);
+            $service->image()->create([
+                'name' => $imagename
+            ]);
+        }
+        return redirect('/services');
     }
 
     /**
@@ -146,8 +149,8 @@ class ServiceController extends Controller
             'editor-ge' => 'required',
             'editor-en' => '',
             'editor-ru' => '',
-            'duration_count' => 'required|between:0,99.99', 
-            'duration_type' => 'required|string', 
+            'duration_count' => 'required|between:0,99.99',
+            'duration_type' => 'required|string',
             'price' => 'required|between:0,99.99',
             'unit-ge' => '',
             'unit-en' => '',
@@ -155,63 +158,63 @@ class ServiceController extends Controller
             'file' => 'image',
             'inventory' => '',
             'quantity' => '',
-         ]);
-     $service->title_ge = $request->input('title_ge');
-     $service->title_en = $request->input('title_en');
-     $service->title_ru = $request->input('title_ru');
-     $service->body_ge = $request->input('editor-ge');
-     $service->body_en = $request->input('editor-en');
-     $service->body_ru = $request->input('editor-ru');
-     $service->duration_count = $request->input('duration_count');
-     $service->duration_type = $request->input('duration_type');
-     $service->unit_ge = $request->input('unit-ge');
-     $service->unit_ru = $request->input('unit-ru');
-     $service->unit_en = $request->input('unit-en');
-     $service->price = intval($request->input('price')*100);
-     $array = array();
-     if($request->input('inventory') && $request->input('quantity')){
-         foreach($request->input('inventory') as $key => $item){
-             $array[] =[
-                'product_id' => $request->input('inventory')[$key],
-                'quantity' => $request->input('quantity')[$key],
-             ];
-         }
-         $service->inventories()->createMany($array);
-     }
-         if($request->input('category-ge') || $request->input('category-en') || $request->input('category-ru')){
-             if($service->category()->first()){
+        ]);
+        $service->title_ge = $request->input('title_ge');
+        $service->title_en = $request->input('title_en');
+        $service->title_ru = $request->input('title_ru');
+        $service->body_ge = $request->input('editor-ge');
+        $service->body_en = $request->input('editor-en');
+        $service->body_ru = $request->input('editor-ru');
+        $service->duration_count = $request->input('duration_count');
+        $service->duration_type = $request->input('duration_type');
+        $service->unit_ge = $request->input('unit-ge');
+        $service->unit_ru = $request->input('unit-ru');
+        $service->unit_en = $request->input('unit-en');
+        $service->price = intval($request->input('price')*100);
+        $array = array();
+        if($request->input('inventory') && $request->input('quantity')){
+            foreach($request->input('inventory') as $key => $item){
+                $array[] =[
+                    'product_id' => $request->input('inventory')[$key],
+                    'quantity' => $request->input('quantity')[$key],
+                ];
+            }
+            $service->inventories()->createMany($array);
+        }
+        if($request->input('category-ge') || $request->input('category-en') || $request->input('category-ru')){
+            if($service->category()->first()){
                 $servicecat = $service->category()->first();
                 $servicecat->title_ge = $request->input('category-ge');
                 $servicecat->title_ru = $request->input('category-ru');
                 $servicecat->title_en = $request->input('category-en');
                 $servicecat->save();
-             }else{
+            }else{
                 $service->category()->create([
                     'title_ge' => $request->input('category-ge'),
                     'title_ru' => $request->input('category-ru'),
                     'title_en' => $request->input('category-en'),
-                 ]);
-             }
-         }elseif(!$request->input('category-ge') & !$request->input('category-en') && !$request->input('category-ru') && $service->category()->first()){
+                ]);
+            }
+        }elseif(!$request->input('category-ge') & !$request->input('category-en') && !$request->input('category-ru') && $service->category()->first()){
             $service->category()->first()->delete();
-         }
-         $service->save();
-         
-         if($request->hasFile('file')){
+        }
+        $service->save();
+
+        if($request->hasFile('file')){
             $imagename = date('Ymhs').$request->file('file')->getClientOriginalName();
             $destination = base_path() . '/storage/app/public/serviceimg';
             $request->file('file')->move($destination, $imagename);
             if($service->first()->image()->first()){
-               $firstimg = $service->first()->image()->first();
-               $firstimg->name = $imagename;
-               $firstimg->save();
+                $firstimg = $service->first()->image()->first();
+                $firstimg->name = $imagename;
+                $firstimg->save();
             }else{
                 $service->first()->image()->create([
                     'name' => $imagename
                 ]);
             }
-         }
-         return redirect('/services');
+        }
+        return redirect('/services');
     }
 
     /**
@@ -229,7 +232,7 @@ class ServiceController extends Controller
             $serv->save();
         }
         $service->deleted_at = Carbon::now('Asia/Tbilisi');
-            $service->save();
+        $service->save();
         return redirect('/services');
     }
 
@@ -245,28 +248,32 @@ class ServiceController extends Controller
     public function getcategory(Request $request){
         $data = Category::where('title', 'like', '%'.$request->input('value').'%')->orderBy('id', 'DESC')->take(4);
 
-         return response()->json($data);
+        return response()->json($data);
     }
+
     //Service Categories
     public function categories(){
         $categories = Category::whereNull('deleted_at')->orderBy('id', 'DESC')->paginate(50);
         return view('theme.template.main.categories', compact('categories'));
     }
+
     public function removecategory($id){
         $Category = Category::findorfail($id);
         $Category->delete_at = Carbon::now('Asia/Tbilisi');
         $Category->save();
         return redirect('/categories');
     }
+
     //Get Unit Name for Inventory Product
     public function getunitname($id){
         $unit = Product::findOrFail($id);
         return response()->json(array('status'=>true, 'data' => $unit->unit));
     }
+
     //Remove Inventory
     public function removeinventory(Request $request){
         $this->validate($request, [
-            'invid' => 'required|integer' 
+            'invid' => 'required|integer'
         ]);
         $inventory = Inventory::findOrFail($request->invid);
         if($inventory->delete()){
@@ -274,10 +281,11 @@ class ServiceController extends Controller
         }
         return;
     }
+
     //Remove Image
     public function removeimage(Request $request){
         $this->validate($request, [
-            'imgid' => 'required|integer' 
+            'imgid' => 'required|integer'
         ]);
         $image = Image::findOrFail($request->imgid);
         if($image->delete()){
